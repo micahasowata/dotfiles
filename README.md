59e0c9618967d432300798145a4363e98d74374a
Micah Asowata <andrewgary963@gmail.com>
2025-05-04 13:18:52 +0100

    current wezterm config

---
+++ README.md
@@ -0,0 +1,11 @@
+# configs
+
+configuration for my wezterm and hopefully my vim too.
+
+copy at your own risk.
+
+to get the configurations to work as expected
+
+clone this repo
+set up symlinks for wezterm and lazyvim.
+symlinks work.
---
+++ wezterm.lua
@@ -0,0 +1,41 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+config.show_tab_index_in_tab_bar = false
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.color_scheme = "Catppuccin Macchiato"
+
+
+config.enable_scroll_bar = true
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+
+config.window_padding = {
+    left = 2,
+    right = 2,
+    top = 0,
+}
+
+-- replace with the absolute path to your background image
+config.window_background_image = "/home/micah/dontdelete/bg.jpg"
+
+config.window_background_image_hsb = {
+    brightness = 0.1,
+
+    hue = 0.8,
+
+    saturation = 0.9,
+}
+
+config.default_cursor_style = "BlinkingUnderline"
+
+return config
