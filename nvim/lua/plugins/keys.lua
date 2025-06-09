6782319b6550f8d36b96715929e4effd5cd779a8
Micah Asowata <andrewgary963@gmail.com>
2025-06-09 14:57:47 +0100

    add keybinding to open terminal

--- nvim/lua/plugins/keys.lua
+++ nvim/lua/plugins/keys.lua
@@ -6,6 +6,7 @@ return {
 			spec = {
 				{ "<leader>ff", "<cmd>FzfLua files<cr>", desc = "Fuzzy Find Files", mode = "n" },
 				{ "<leader>fo", "<cmd>Oil<cr>", desc = "Manipulate filesystem", mode = "n" },
+				{ "<leader>ft", "<cmd>terminal<cr>", desc = "Open terminal", mode = "n" },
 				{ "<C-s>", "<cmd>w<cr>", desc = "Save current file", mode = "n" },
 				{ "<C-s>", "<cmd>w<cr>", desc = "Save current file (insert mode)", mode = "i" },
 				{ "<leader>q", "<cmd>wq<cr>", desc = "Quit (it saves file too)", mode = "n" },
