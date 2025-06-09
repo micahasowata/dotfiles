be73c11b8a02552e303f6ebc21a0932df5a6c16d
Micah Asowata <andrewgary963@gmail.com>
2025-06-09 16:03:19 +0100

    add keybinding to run `:Lazy`

--- nvim/lua/plugins/keys.lua
+++ nvim/lua/plugins/keys.lua
@@ -4,9 +4,12 @@ return {
 		event = "VeryLazy",
 		opts = {
 			spec = {
+				{ "<leader>l", "<cmd>Lazy<cr>", desc = "Open Lazy", mode = "n" },
+
 				{ "<leader>ff", "<cmd>FzfLua files<cr>", desc = "Fuzzy Find Files", mode = "n" },
 				{ "<leader>fo", "<cmd>Oil<cr>", desc = "Manipulate filesystem", mode = "n" },
 				{ "<leader>ft", "<cmd>terminal<cr>", desc = "Open terminal", mode = "n" },
+
 				{ "<C-s>", "<cmd>w<cr>", desc = "Save current file", mode = "n" },
 				{ "<C-s>", "<cmd>w<cr>", desc = "Save current file (insert mode)", mode = "i" },
 				{ "<leader>q", "<cmd>wq<cr>", desc = "Quit (it saves file too)", mode = "n" },
