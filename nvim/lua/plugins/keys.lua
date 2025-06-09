9e76545d132ea624c1a6dfa423b6bcb605a6c261
Micah Asowata <andrewgary963@gmail.com>
2025-06-09 14:47:15 +0100

    add `conform`, `which-key`, and `noice`

---
+++ nvim/lua/plugins/keys.lua
@@ -0,0 +1,26 @@
+return {
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			spec = {
+				{ "<leader>ff", "<cmd>FzfLua files<cr>", desc = "Fuzzy Find Files", mode = "n" },
+				{ "<leader>fo", "<cmd>Oil<cr>", desc = "Manipulate filesystem", mode = "n" },
+				{ "<C-s>", "<cmd>w<cr>", desc = "Save current file", mode = "n" },
+				{ "<C-s>", "<cmd>w<cr>", desc = "Save current file (insert mode)", mode = "i" },
+				{ "<leader>q", "<cmd>wq<cr>", desc = "Quit (it saves file too)", mode = "n" },
+			},
+		},
+
+		keys = {
+
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Hello from which-key",
+			},
+		},
+	},
+}
